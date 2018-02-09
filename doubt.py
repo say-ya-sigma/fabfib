edfa88d9c4da9f872840229022e66d869421c2b5
sigma <masingan43@gmail.com>
2018-02-10 06:07:27 +0900

    doubt.py basiclogic first write

--- doubt.py
+++ doubt.py
@@ -1,5 +1,6 @@
 import numpy as np
 from abc import ABCMeta, abstractmethod
+import numpy as np
 
 
 class DoubtStrategy(object):
@@ -9,6 +10,7 @@ class DoubtStrategy(object):
 		self.Number = Number
 		self.Call = Call
 		self.DiscardCount = DiscardCount
+		self.Doubt = False
 
 	@abstractmethod
 	def doubt_check(self):
@@ -19,6 +21,9 @@ class BasicLogic(DoubtStrategy):
 		super().__init__(Number,Call,DiscardCount)
 
 	def doubt_check(self):
-		pass ## todo, doubt_check design is not defined
-		
+		DoubtIndex = (np.random.rand() * 0.1 + 0.9) * ((Call - 700 / 300) ** 2)
+		if DoubtIndex > 0.8:
+			self.Doubt = True
+
+		return self.Doubt
 
